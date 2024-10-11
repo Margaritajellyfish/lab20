b83c022b40ac8a346eec51ca4e5a6d0a62505c29
王辰毓 <margarita@MacBook-Pro-3.local>
2024-10-10 20:54:44 -0700

    3

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include<ctime>
 
 using namespace std;
 const int SIZE = 3;
@@ -12,9 +13,21 @@ public:
     // constructors
     Chair() {
         const int MIN = 10000, MAX = 99999;
+        srand(time(0) + rand());
         legs = (rand()%2+3);
+        prices = new double[SIZE];
         for (int i = 0; i < SIZE; i++)
             prices[i] = (rand() % (MAX-MIN+1) + MIN) / (double) 100;
+    }
+    Chair(int l, double p[SIZE]) {
+        legs = l;
+        prices = new double[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            prices[i] = p[i];
+        }
+    }
+    ~Chair() {
+        delete[] prices;
     }
     // setters and getters
     int getLegs(){ return legs; }
@@ -34,6 +47,8 @@ public:
         cout << endl << "Historical avg price: " << getAveragePrices();
         cout << endl << endl;
     }
+    
+
 };
 
 int main() {
@@ -44,7 +59,8 @@ int main() {
     chairPtr->print();
 
     //creating dynamic chair object with constructor
-    Chair *livingChair = new Chair(3);
+    double livingPrices[SIZE] = { 525.25, 434.34, 252.52 };
+    Chair* livingChair = new Chair(3, livingPrices);
     livingChair->print();
     delete livingChair;
     livingChair = nullptr;
@@ -55,5 +71,7 @@ int main() {
     for (int i = 0; i < SIZE; i++)
         collection[i].print();
     
+    delete[] collection;
+    collection = nullptr;
     return 0;
 }
