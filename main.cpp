2937b7c393331c8fc3f07eb5a9fb891e8ab7e99c
王辰毓 <margarita@MacBook-Pro-3.local>
2024-10-10 20:39:27 -0700

    2

--- main.cpp
+++ main.cpp
@@ -11,26 +11,14 @@ private:
 public:
     // constructors
     Chair() {
-        prices = new double[SIZE];
-        legs = 0;
+        const int MIN = 10000, MAX = 99999;
+        legs = (rand()%2+3);
         for (int i = 0; i < SIZE; i++)
-            prices[i] = 0;
+            prices[i] = (rand() % (MAX-MIN+1) + MIN) / (double) 100;
     }
-    Chair(int l) {
-        prices = new double[SIZE];
-        legs = l;
-        for (int i = 0; i < SIZE; i++)
-            prices[i] = 0;
-    }
-
     // setters and getters
-    void setLegs(int l)      { legs = l; }
-    int getLegs()            { return legs; }
-
-    void setPrices(double p1, double p2, double p3) { 
-        prices[0] = p1; prices[1] = p2; prices[2] = p3; 
-    }
-
+    int getLegs(){ return legs; }
+    
     double getAveragePrices() {
         double sum = 0;
         for (int i = 0; i < SIZE; i++)
@@ -53,25 +41,17 @@ int main() {
 
     //creating pointer to first chair object
     Chair *chairPtr = new Chair;
-    chairPtr->setLegs(4);
-    chairPtr->setPrices(121.21, 232.32, 414.14);
     chairPtr->print();
 
     //creating dynamic chair object with constructor
     Chair *livingChair = new Chair(3);
-    livingChair->setPrices(525.25, 434.34, 252.52);
     livingChair->print();
     delete livingChair;
     livingChair = nullptr;
 
     //creating dynamic array of chair objects
     Chair *collection = new Chair[SIZE];
-    collection[0].setLegs(4);
-    collection[0].setPrices(441.41, 552.52, 663.63);
-    collection[1].setLegs(4);
-    collection[1].setPrices(484.84, 959.59, 868.68);
-    collection[2].setLegs(4);
-    collection[2].setPrices(626.26, 515.15, 757.57);
+   
     for (int i = 0; i < SIZE; i++)
         collection[i].print();
     
